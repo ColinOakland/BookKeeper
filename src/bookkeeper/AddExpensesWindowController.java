474a926d29df299a239edf198623953421c7b95d
Colin Murphy <colinmurphy@oakland.edu>
2019-02-13 12:31:36 -0500

    Added functionality so that data can be inputted and recorded from the Add Expenses window. Data is now written to record.csv in a comma seperated value file.

---
+++ src/bookkeeper/AddExpensesWindowController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookkeeper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.*;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author colin
+ */
+public class AddExpensesWindowController implements Initializable {
+    
+    @FXML
+    private Button addExpensesDoneButton;
+    
+    @FXML
+    private TextField purposeTextField;
+    @FXML
+    private TextField addExpenseshowMuchTextField;
+    @FXML
+    private DatePicker date;
+    @FXML
+    private TextField AddExpensesCategory;
+    
+    @FXML
+    private void addExpensesDoneButtonAction(ActionEvent event) throws IOException
+    {
+        FileWriter writer;
+        writer = new FileWriter("record.csv", true);
+        
+        //get all the data from fields, so we can write them
+        String purpose = purposeTextField.getText();
+        String howMuch = addExpenseshowMuchTextField.getText();
+        LocalDate expenseDate = date.getValue();
+        String category = AddExpensesCategory.getText();
+        
+        
+        //write the data to a csv file which is in the root directory as record.txt
+        writer.append("expense," + purpose + "," + howMuch + "," + expenseDate + ","+category+"\n");
+        
+        //close the file
+        writer.close();
+        
+        Stage stage = (Stage) addExpensesDoneButton.getScene().getWindow();
+        stage.close();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
--- src/bookkeeper/BudgetBookkeepingController.java
+++ src/bookkeeper/BudgetBookkeepingController.java
@@ -11,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class BudgetBookkeepingController implements Initializable {
@@ -60,7 +61,7 @@ public class BudgetBookkeepingController implements Initializable {
     }
     
 
-            
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
